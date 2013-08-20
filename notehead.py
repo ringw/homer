63eebedf1efa1c3d9575af55d73fe500ca0c4622
Dan Ringwalt <dringw@gmail.com>
2013-08-20 11:27:51 -0500

    Determine notehead from multiple models

--- notehead.py
+++ notehead.py
@@ -64,22 +64,23 @@ class NoteheadsTask:
     glyph_proj_num = np.repeat(glyph_nums, map(len, projections))
     glyph_proj = np.concatenate(projections)
     glyph_proj **= 2
-    self.model_glyph_proj = glyph_proj
-    self.model_glyph_proj_num = glyph_proj_num
-    self.model_glyph_mask = glyphs
-    self.model_glyphs = individual_glyphs
+    self.candidate_glyph_proj = glyph_proj
+    self.candidate_glyph_proj_num = glyph_proj_num
+    self.candidate_glyph_mask = glyphs
+    self.candidate_glyphs = individual_glyphs
 
-  def choose_model_glyph(self):
-    dy = np.empty_like(self.model_glyph_proj)
+  def choose_model_glyphs(self):
+    dy = np.empty_like(self.candidate_glyph_proj)
     dy[[0, -1]] = 0
-    dy[1:-1] = self.model_glyph_proj[2:] - self.model_glyph_proj[:-2]
-    self.glyph_proj_deriv = dy
+    dy[1:-1] = self.candidate_glyph_proj[2:] - self.candidate_glyph_proj[:-2]
     xs = np.arange(self.page.staff_space)
     ys = xs[:, None] + np.arange(len(dy) - len(xs))[None, :]
     fit, res, rank, sing, rcond = np.polyfit(xs, dy[ys], 1, full=True)
     # Candidates must have small negative slope and intersect near center
     # of interval
     candidates = (fit[0] < -self.page.staff_space/2) & (fit[0] > -2*self.page.staff_space)
+    # Find intersection of fitted line at local maximum of projection
+    # (predicted x-center of notehead)
     denom = (-fit[0]).astype(np.double)
     denom[denom == 0] = 1e-10
     local_maximum = fit[1]/denom
@@ -91,75 +92,111 @@ class NoteheadsTask:
     self.proj_fit = fit
     self.proj_residual = res
     self.proj_candidates = candidates
-    proj_x = np.argmin(res[candidates])
-    self.candidate_glyph = self.model_glyph_proj_num[candidates][proj_x]
-    glyph_proj_start = np.where(self.model_glyph_proj_num[candidates] == self.candidate_glyph)[0][0]
-    self.candidate_x = local_maximum[candidates][proj_x] + proj_x - glyph_proj_start
-    return (self.candidate_glyph, self.candidate_x)
+    # Choose candidate xs which must have a residual below RES_MAX
+    # (may include multiple x values for the same glyph)
+    RES_MAX = float(len(xs)) * 1000 # XXX
+    res[~candidates] = RES_MAX + 1
+    candidate_xs = np.argsort(res)
+    bad_xs, = np.where(res[candidate_xs] > RES_MAX)
+    if len(bad_xs):
+      candidate_xs = candidate_xs[:bad_xs[0]]
+    # Choose model xs from candidates which are the best candidate
+    # for each unique glyph
+    #model_glyphs = self.candidate_glyph_proj_num[candidate_xs]
+    model_glyphs, model_proj_xs = np.unique(self.candidate_glyph_proj_num[candidate_xs], return_index=True)
+    # Index model_proj_xs back into candidate_glyph_proj_num
+    model_proj_xs = candidate_xs[model_proj_xs]
+    # Find actual x for each glyph from model_proj_xs
+    #model_glyphs = self.candidate_glyph_proj_num[model_proj_xs]
+    model_xs = model_proj_xs.copy()
+    candidate_glyphs, glyph_proj_starts = np.unique(self.candidate_glyph_proj_num, return_index=True)
+    glyph_num_start = np.zeros(self.page.num_glyphs, dtype=int)
+    glyph_num_start[candidate_glyphs] = glyph_proj_starts
+    # Index of model_proj_xs into candidate_glyph_proj_num
+    candidate_nums = np.where(candidate_xs)
+    model_xs -= glyph_num_start[model_glyphs]
+    model_xs += local_maximum[model_xs]
+    # Keep only sane values of model_xs
+    xs_sane = ((0 <= model_xs)
+               & (model_xs < np.diff(self.page.glyph_bounds[model_glyphs, 1])[0]))
+    self.model_glyphs = model_glyphs[xs_sane]
+    self.model_xs = model_xs[xs_sane]
+    return (self.model_glyphs, self.model_xs)
 
-  def fit_model_ellipse(self):
-    # Extract border of model notehead
-    glyph_box = self.page.glyph_boxes[self.candidate_glyph]
-    glyph_section = self.page.labels[glyph_box]
-    gnum = self.candidate_glyph + 1
-    glyph = (glyph_section == gnum).astype(int)
-    proj = np.sum(glyph, axis=0) ** 2
-    glyph_border = glyph & (convolve2d(glyph,
-                                       [[1,1,1], [1,0,1], [1,1,1]],
-                                       mode='same') < 8)
+  def fit_model_ellipses(self):
+    self.model_ellipses = []
+    for candidate_glyph, candidate_x in zip(self.model_glyphs, self.model_xs):
+      # Extract border of model notehead
+      glyph_box = self.page.glyph_boxes[candidate_glyph]
+      glyph_section = self.page.labels[glyph_box]
+      gnum = candidate_glyph + 1
+      glyph = (glyph_section == gnum).astype(int)
+      proj = np.sum(glyph, axis=0) ** 2
+      glyph_border = glyph & (convolve2d(glyph,
+                                         [[1,1,1], [1,0,1], [1,1,1]],
+                                         mode='same') < 8)
 
-    # Choose smooth sections of vertical projection for least squares fit,
-    # based on second derivative of the Gaussian
-    deriv = gaussian_filter(proj, 1, order=2)
-    xs = np.zeros_like(proj, dtype=bool)
-    X_DIST = self.page.staff_space/2
-    X0 = int(np.rint(self.candidate_x))
-    xs[max(X0 - X_DIST, 0):X0 + X_DIST] = True
-    xs &= np.abs(deriv) < self.page.staff_space
+      # Choose smooth sections of vertical projection for least squares fit,
+      # based on second derivative of the Gaussian
+      deriv = gaussian_filter(proj, 1, order=2)
+      xs = np.zeros_like(proj, dtype=bool)
+      X_DIST = self.page.staff_space/2
+      X0 = int(np.rint(candidate_x))
+      xs[max(X0 - X_DIST, 0):X0 + X_DIST] = True
+      xs &= np.abs(deriv) < self.page.staff_space
 
-    # Get all border x and y coordinates for least squares fit
-    y_coords, x_coords = np.where(glyph_border & xs[None, :])
-    # Algorithm adapted from:
-    # http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.1.7559
-    D1 = np.column_stack((x_coords**2, x_coords*y_coords, y_coords**2))
-    D2 = np.column_stack((x_coords, y_coords, np.ones(len(x_coords))))
-    S1 = D1.astype(np.double).T.dot(D1)
-    S2 = D1.astype(np.double).T.dot(D2)
-    S3 = D2.astype(np.double).T.dot(D2)
-    T = -np.linalg.inv(S3).dot(S2.T)
-    M = S1 + S2.dot(T) # reduced scatter matrix
-    M = np.vstack([M[2] / 2.0, -M[1], M[0] / 2.0]) # premultiply by inv(C1)
-    evals, evecs = np.linalg.eig(M)
-    cond = 4 * (evecs[0] * evecs[2]) - evecs[1]**2 # a^T . C . a
-    a1 = evecs[:, np.where(cond > 0)[0][0]]
-    params = np.concatenate((a1, T.dot(a1)))
-    x0, y0, a, b, t = ellipse_general_to_standard(*params)
-    print x0,y0,a,b,t
-    if DEBUG():
-      import pylab as P
-      im = np.zeros(glyph_border.shape + (3,), dtype=bool)
-      im[..., 2] = glyph
-      # Draw ellipse
-      draw_ts = np.linspace(0, 2*np.pi, 100)
-      draw_xs = x0 + a*np.cos(draw_ts)*np.cos(t) - b*np.sin(draw_ts)*np.sin(t)
-      draw_ys = y0 + a*np.cos(draw_ts)*np.sin(t) + b*np.sin(draw_ts)*np.cos(t)
-      draw_xs = np.rint(draw_xs).astype(int)
-      draw_ys = np.rint(draw_ys).astype(int)
-      in_bounds = ((0 <= draw_xs) & (draw_xs < im.shape[1])
-                   & (0 <= draw_ys) & (draw_ys < im.shape[0]))
-      draw_xs = draw_xs[in_bounds]
-      draw_ys = draw_ys[in_bounds]
-      im[draw_ys, draw_xs, 0] = True
-      P.imshow(im)
-      P.show()
+      if DEBUG():
+        import pylab as P
+        im = np.zeros(glyph.shape + (3,), dtype=bool)
+        im[..., 2] = glyph
+        im[:, xs, 0] = True
+        im[:, X0, 1] = True
+        P.imshow(im)
+        P.show()
 
-  def model_ellipse_glyph(self):
-    self.choose_model_glyph_candidates()
-    self.choose_model_glyph()
+      # Get all border x and y coordinates for least squares fit
+      y_coords, x_coords = np.where(glyph_border & xs[None, :])
+      # Algorithm adapted from:
+      # http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.1.7559
+      D1 = np.column_stack((x_coords**2, x_coords*y_coords, y_coords**2))
+      D2 = np.column_stack((x_coords, y_coords, np.ones(len(x_coords))))
+      S1 = D1.astype(np.double).T.dot(D1)
+      S2 = D1.astype(np.double).T.dot(D2)
+      S3 = D2.astype(np.double).T.dot(D2)
+      T = -np.linalg.inv(S3).dot(S2.T)
+      M = S1 + S2.dot(T) # reduced scatter matrix
+      M = np.vstack([M[2] / 2.0, -M[1], M[0] / 2.0]) # premultiply by inv(C1)
+      evals, evecs = np.linalg.eig(M)
+      cond = 4 * (evecs[0] * evecs[2]) - evecs[1]**2 # a^T . C . a
+      a1 = evecs[:, np.where(cond > 0)[0][0]]
+      params = np.concatenate((a1, T.dot(a1)))
+      x0, y0, a, b, t = ellipse_general_to_standard(*params)
+      self.model_ellipses.append((x0, y0, a, b, t))
+      if DEBUG():
+        import pylab as P
+        im = np.zeros(glyph.shape + (3,), dtype=bool)
+        im[..., 2] = glyph
+        # Draw ellipse
+        draw_ts = np.linspace(0, 2*np.pi, 100)
+        draw_xs = x0 + a*np.cos(draw_ts)*np.cos(t) - b*np.sin(draw_ts)*np.sin(t)
+        draw_ys = y0 + a*np.cos(draw_ts)*np.sin(t) + b*np.sin(draw_ts)*np.cos(t)
+        draw_xs = np.rint(draw_xs).astype(int)
+        draw_ys = np.rint(draw_ys).astype(int)
+        in_bounds = ((0 <= draw_xs) & (draw_xs < im.shape[1])
+                     & (0 <= draw_ys) & (draw_ys < im.shape[0]))
+        draw_xs = draw_xs[in_bounds]
+        draw_ys = draw_ys[in_bounds]
+        im[draw_ys, draw_xs, 0] = True
+        P.imshow(im)
+        P.show()
+    self.notehead_model = np.median(self.model_ellipses, axis=0)
+    self.page.notehead_model = self.notehead_model
+    print self.notehead_model
 
   def process(self):
-    self.model_ellipse_glyph()
-    self.fit_model_ellipse()
+    self.choose_model_glyph_candidates()
+    self.choose_model_glyphs()
+    self.fit_model_ellipses()
 
   def color_image(self):
     pass
