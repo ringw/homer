d12da01c6f1d9a009157b11e2841787b36a5f21f
Dan Ringwalt <dringw@gmail.com>
2013-09-04 21:29:35 -0400

    Moved ellipse functions to new geometry.ellipse module

--- notehead.py
+++ notehead.py
@@ -1,38 +1,7 @@
 import numpy as np
 from debug import DEBUG
 from scipy import ndimage
-
-def ellipse_general_to_standard(A, B, C, D, E, F):
-  """
-  Convert an ellipse in general form:
-    Ax^2 + Bxy + Cy^2 + Dx + Ey + F = 0
-  To standard form:
-    ((x - x0) cos t - (y - y0) sin t)^2/a^2
-    + ((x - x0) sin t + (y - y0) cos t)^2/b^2 = 1
-  The ellipse has center (x0, y0), major and minor semi-axes a and b,
-  and the angle to the semi-major axis is t.
-
-  Parameters: A, B, C, D, E, F
-  Returns: x0, y0, a, b, t
-  """
-  A, B, C, D, E, F = map(float, [A, B, C, D, E, F])
-  # Matrix representation of conic section
-  AQ = np.array([[A, B/2, D/2], [B/2, C, E/2], [D/2, E/2, F]])
-  A33 = AQ[0:2, 0:2]
-  # Formula for center
-  x0, y0 = np.linalg.inv(A33).dot([-D/2, -E/2])
-  # Each eigenvector of A33 lies along one of the axes
-  evals, evecs = np.linalg.eigh(A33)
-  # Semi-axes from reduced canonical equation
-  a, b = np.sqrt(-np.linalg.det(AQ)/(np.linalg.det(A33)*evals))
-  # Return major axis as "a" and angle to major axis between -pi/2 and pi/2
-  t = np.arctan2(evecs[1,0], evecs[0,0]) # angle of axis "a"
-  if b > a:
-    a, b = b, a
-    t += np.pi/2
-  if   t < -np.pi/2: t += np.pi
-  elif t >  np.pi/2: t -= np.pi
-  return (x0, y0, a, b, t)
+from geometry.ellipse import least_squares_fit, general_to_standard
 
 class NoteheadsTask:
   def __init__(self, page):
@@ -157,22 +126,8 @@ class NoteheadsTask:
       y_coords, x_coords = np.where(glyph_border & xs[None, :])
       # Algorithm adapted from:
       # http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.1.7559
-      D1 = np.column_stack((x_coords**2, x_coords*y_coords, y_coords**2))
-      D2 = np.column_stack((x_coords, y_coords, np.ones(len(x_coords))))
-      S1 = D1.astype(np.double).T.dot(D1)
-      S2 = D1.astype(np.double).T.dot(D2)
-      S3 = D2.astype(np.double).T.dot(D2)
-      T = -np.linalg.inv(S3).dot(S2.T)
-      M = S1 + S2.dot(T) # reduced scatter matrix
-      M = np.vstack([M[2] / 2.0, -M[1], M[0] / 2.0]) # premultiply by inv(C1)
-      # M should be well-conditioned
-      if np.linalg.cond(M) > 1e4:
-        continue
-      evals, evecs = np.linalg.eig(M)
-      cond = 4 * (evecs[0] * evecs[2]) - evecs[1]**2 # a^T . C . a
-      a1 = evecs[:, np.where(cond > 0)[0][0]]
-      params = np.concatenate((a1, T.dot(a1)))
-      x0, y0, a, b, t = ellipse_general_to_standard(*params)
+      A, B, C, D, E, F = least_squares_fit(x_coords, y_coords)
+      x0, y0, a, b, t = general_to_standard(A, B, C, D, E, F)
       self.model_ellipses.append((x0, y0, a, b, t))
       if DEBUG():
         import pylab as P
