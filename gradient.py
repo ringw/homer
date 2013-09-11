016185e2338ce55ac20b251325c5da7a90daf8e8
Dan Ringwalt <dringw@gmail.com>
2013-09-10 22:02:19 -0400

    Cleanup

--- gradient.py
+++ gradient.py
@@ -5,8 +5,8 @@ from scipy.ndimage.filters import convolve, gaussian_filter1d
 def gaussian(im, sigma=3.0):
   SHAPE = (2,) + im.shape
   gaussians = np.zeros(SHAPE, dtype=np.double)
-  gaussian_filter1d(im, sigma, axis=0, order=1, output=gaussians[0])
-  gaussian_filter1d(im, sigma, axis=1, order=1, output=gaussians[1])
+  gaussian_filter1d(im, sigma, mode='constant', axis=0, order=1, output=gaussians[0])
+  gaussian_filter1d(im, sigma, mode='constant', axis=1, order=1, output=gaussians[1])
   output = np.zeros(SHAPE, dtype=np.double)
   output[0] = np.arctan2(gaussians[0], gaussians[1])
   output[1] = np.sqrt(gaussians[0]**2 + gaussians[1]**2)
@@ -18,20 +18,4 @@ class GradientTask:
   def process(self):
     self.page.gradient = gaussian(self.page.im, sigma=self.page.staff_thick)
   def color_image(self):
-    return
-    #import matplotlib
-    import matplotlib.pyplot as plt
-    #cmap = matplotlib.cm.jet
-    #cmap.set_bad('w',1.)
-    border = (self.page.im
-              & (convolve(self.page.im, np.ones((3,3)), mode='constant') < 9))
-    by,bx = np.where(border)
-    img = np.zeros(border.shape)
-    img[:] = 10
-    img[by,bx] = self.gradient[0,by,bx]
-    #mask = np.ma.masked_where(self.gaussian[0], mask=border)
-    #plt.imshow(img[0, 500:1000, 500:1000])
-    #plt.colorbar()
-    #plt.show()
-    h = np.histogram(self.gaussian[0].ravel(), bins=360, range=(-np.pi,np.pi))
-    print h
+    pass
