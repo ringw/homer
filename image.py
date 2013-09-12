204912fb1c598748d2d4d3dfc1ee604bf3c6b7ff
Dan Ringwalt <dringw@gmail.com>
2013-09-12 10:47:57 -0400

    Multi-page PDF and faster image loading

--- image.py
+++ image.py
@@ -1,5 +1,7 @@
 import Image
 import tempfile
+import os
+import shutil
 from StringIO import StringIO
 import subprocess
 import numpy as np
@@ -8,9 +10,13 @@ import page
 def image_array(f):
   im = Image.open(f)
   im = im.convert('1')
-  # White: 0, Black: 1
-  arr = 1 - np.array(list(im.getdata()), dtype=np.int)/255
-  return (arr.reshape(im.size[1], im.size[0]), im.convert('RGB'))
+  im = im.convert('L')
+  bytestring = im.tostring()
+  pixels = np.fromstring(bytestring, dtype=np.uint8)
+  pixels = pixels.reshape((im.size[1], im.size[0]))
+  # Swap pixels so colored pixels are 1
+  np.logical_not(pixels, output=pixels)
+  return (pixels, im.convert('RGB'))
 
 # Open image or multi-page PDF, return list of pages
 def read_pages(path):
@@ -19,15 +25,21 @@ def read_pages(path):
   path.seek(0)
   if path.read(4) == '%PDF':
     path.seek(0)
-    outFile = tempfile.NamedTemporaryFile(suffix='.png')
+    #outFile = tempfile.NamedTemporaryFile(suffix='.png')
+    tmpDir = tempfile.mkdtemp(prefix='moonshineTemp')
+    tmpFormat = os.path.join(tmpDir, "page%05d.png")
     ARGS = ['gs', '-dBATCH', '-dNOPAUSE', '-dSAFER',
             '-sDEVICE=pngmono', '-r300',
-            '-sOutputFile=' + outFile.name,
+            '-sOutputFile=' + tmpFormat,
             path.name]
     gs = subprocess.Popen(ARGS)
     gs.wait()
     if gs.returncode != 0: return False
-    outFile.seek(0)
-    return [page.Page(*image_array(outFile))]
-  path.seek(0)
-  return [page.Page(*image_array(path))]
+    pages = []
+    for filename in sorted(os.listdir(tmpDir)):
+      pages.append(page.Page(*image_array(os.path.join(tmpDir, filename))))
+    shutil.rmtree(tmpDir)
+    return pages
+  else:
+    path.seek(0)
+    return [page.Page(*image_array(path))]
