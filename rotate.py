a383b37c85b0de2f15cdb9735c749ff03c6bfab8
Dan Ringwalt <dringw@gmail.com>
2013-09-24 23:35:47 -0400

    Fixed rotate

--- rotate.py
+++ rotate.py
@@ -1,4 +1,5 @@
 from numpy import *
+from scipy import ndimage
 import Image
 
 class RotateTask:
@@ -8,15 +9,23 @@ class RotateTask:
     self.pil_im = Image.fromstring('L', page.im.shape[::-1], im_string)
 
   def test_angles(self, ts):
+    mask = empty(self.page.staff_thick)
+    mask[:] = -2
+    mask[len(mask)/2 - self.page.staff_space:len(mask)/2 + self.page.staff_space] = 1
     rotated_ims = [self.pil_im.rotate(t * 180.0 / pi) for t in ts]
     all_im_string = ''.join([im.tostring() for im in rotated_ims])
     np_ims = fromstring(all_im_string, dtype=uint8).reshape((len(ts),) + self.page.im.shape)
-    scores = sum(sum(np_ims, axis=-1) ** 2, axis=-1)
+    # Horizontal projection
+    im_proj = sum(np_ims, axis=-1)
+    # Convolve along y-axis
+    im_convolve = ndimage.convolve(im_proj, mask[None,:])
+    im_convolve[im_convolve < 0] = 0
+    scores = sum(im_convolve ** 2, axis=-1)
     return scores, np_ims
 
   def rotate_image(self):
     # Determine direction of rotation
-    scores, _ = self.test_angles([-pi/180, 0, pi/180])
+    scores, _ = self.test_angles([-pi/720, 0, pi/720])
     left, right, straight = scores
     dt = 0
     if left > straight and straight > right:
@@ -26,7 +35,7 @@ class RotateTask:
     # If needs to be rotated, determine rotation to nearest degree
     rotate_base = 0
     if dt:
-      rotate_angles = arange(1, 11) * dt
+      rotate_angles = arange(11) * dt
       scores, _ = self.test_angles(rotate_angles)
       rotate_base = rotate_angles[argmax(scores)]
     # Determine angle to nearest 0.05 degree
