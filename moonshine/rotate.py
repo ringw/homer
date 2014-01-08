3601f2068a3ebc256eb0e92e7129eea5de9cc0ea
Dan Ringwalt <dringw@gmail.com>
2014-01-07 21:57:30 -0600

    Cleaned up rotate

--- moonshine/rotate.py
+++ moonshine/rotate.py
@@ -1,6 +1,4 @@
-from numpy import *
 import numpy as np
-from scipy import ndimage
 from PIL import Image
 
 class RotateTask:
@@ -10,26 +8,36 @@ class RotateTask:
 
   def test_angles(self, ts):
     if self.pil_im is None:
-      im_string = self.page.im.astype(uint8).tostring()
+      im_string = self.page.im.astype(np.uint8).tostring()
       self.pil_im = Image.frombytes('L', self.page.im.shape[::-1], im_string)
 
-    rotated_ims = [self.pil_im.rotate(t * 180.0 / pi) for t in ts]
-    all_im_string = ''.join([im.tobytes() for im in rotated_ims])
-    np_ims = fromstring(all_im_string, dtype=uint8).reshape((len(ts),) + self.page.im.shape)
-    # Horizontal projection
-    im_proj = np.sum(np_ims, axis=-1)
-    scores = np.sum(im_proj ** 2, axis=-1)
-    return scores, np_ims
+    scores = np.empty(len(ts))
+    best_score = 0
+    best_im = None
+    for ind, t in enumerate(ts):
+      rotated = self.pil_im.rotate(t * 180.0 / np.pi)
+      im_string = rotated.tobytes()
+      # Score is square of vertical projection
+      # If staff is lined up nearly horizontally, score will be maximized
+      im = (np.fromstring(im_string, dtype=np.uint8)
+              .reshape(self.page.im.shape))
+      projection = im.sum(axis=-1)
+      score = np.sum(projection ** 2, axis=-1)
+      scores[ind] = score
+      if score > best_score:
+        best_score = score
+        best_im = im
+    return scores, best_im
 
   def rotate_image(self):
     # Determine direction of rotation
-    scores, _ = self.test_angles([-pi/720, 0, pi/720])
+    scores, _ = self.test_angles([-np.pi/720, 0, np.pi/720])
     left, right, straight = scores
     dt = 0
     if left > straight and straight > right:
-      dt = -pi/180
+      dt = -np.pi/180
     elif left < straight and straight < right:
-      dt = pi/180
+      dt = np.pi/180
     # If needs to be rotated, determine rotation to nearest degree
     rotate_base = 0
     if dt:
@@ -37,17 +45,13 @@ class RotateTask:
       scores, _ = self.test_angles(rotate_angles)
       rotate_base = rotate_angles[argmax(scores)]
     # Determine angle to nearest 0.05 degree
-    rotate_candidates = linspace(-pi/360, pi/360, 21) + rotate_base
-    scores, ims = self.test_angles(rotate_candidates)
-    best_angle = argmax(scores)
-    return ims[best_angle], rotate_candidates[best_angle]
+    rotate_candidates = np.linspace(-np.pi/360, np.pi/360, 21) + rotate_base
+    scores, im = self.test_angles(rotate_candidates)
+    best_angle = np.argmax(scores)
+    return im, float(rotate_candidates[best_angle])
 
   def process(self):
     self.page.im, self.t = self.rotate_image()
-    self.page.colored = Image.frombytes('L', (self.page.im.shape[1],
-                                              self.page.im.shape[0]),
-                                        (self.page.im*255).tostring()) \
-                             .convert('RGBA')
     # Force page to reload runlength encoding and spacing
     self.page.get_runlength_encoding()
     self.page.get_spacing()
