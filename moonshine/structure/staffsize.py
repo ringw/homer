53248429ff33b1996baf89fb54b21d985d2d6f74
Dan Ringwalt <dringw@gmail.com>
2014-08-24 19:10:38 -0400

    Separate dark_runs and light_runs functions

--- moonshine/structure/staffsize.py
+++ moonshine/structure/staffsize.py
@@ -5,11 +5,26 @@ import logging
 
 prg = build_program("runhist")
 
-def staffsize(page):
+def dark_runs(img):
     dark_run = thr.empty_like(Type(np.int32, 64))
     dark_run.fill(0)
-    prg.dark_hist(page.img, dark_run, global_size=page.img.shape)
-    dark_run = dark_run.get()
+    prg.dark_hist(img, dark_run, global_size=img.shape)
+    return dark_run.get()
+
+def light_runs(page, img=None):
+    if img is None:
+        img = page.img
+    # Find light run lengths, filtering using staff_thick
+    light_run = thr.empty_like(Type(np.int32, 64))
+    light_run.fill(0)
+    prg.light_hist(img, np.int32(page.staff_thick), light_run,
+                   global_size=img.shape)
+    return light_run.get()
+
+def staffsize(page, img=None):
+    if img is None:
+        img = page.img
+    dark_run = dark_runs(img)
 
     # Assume uniform staff thickness
     staff_thick = np.argmax(dark_run)
@@ -17,32 +32,27 @@ def staffsize(page):
         logging.warn("Unreasonable staff_thick value: " + str(staff_thick))
         page.staff_thick = page.staff_space = page.staff_dist = None
         return None
+    else:
+        page.staff_thick = int(staff_thick)
 
-    # Find light run lengths, filtering using staff_thick
-    light_run = thr.empty_like(dark_run)
-    light_run.fill(0)
-    prg.light_hist(page.img, np.int32(staff_thick), light_run,
-                   global_size=page.img.shape)
-    light_run = light_run.get()
-
+    light_run = light_runs(page, img)
     # Multiple staff space sizes are possible for different instruments
-    space = light_run > light_run.max() / 5.0
-    space_label, num_spaces = util.label_1d(space)
-    space_vals = np.rint(util.center_of_mass_1d(space_label)).astype(int)
-    space_vals = space_vals[(staff_thick * 2 < space_vals)
-                            & (space_vals < 50)]
+    diff = np.diff(light_run)
+    is_max = (diff[:-1] > 0) & (diff[1:] < 0)
+    thresh = light_run[1:-1] > (light_run.max() / 10)
+    space_vals = np.where(is_max & thresh)[0] + 1
+    space_vals = space_vals[staff_thick * 2 < space_vals]
     if len(space_vals) == 0:
         logging.warn("No staves detected")
         staff_space = None
         staff_dist = None
     elif len(space_vals) == 1:
-        staff_space = space_vals[0]
-        staff_dist = staff_space + staff_thick
+        staff_space = int(space_vals[0])
+        staff_dist = int(staff_space + staff_thick)
     else:
-        staff_space = tuple(space_vals)
-        staff_dist = tuple(space_vals + staff_thick)
+        staff_space = tuple(space_vals.tolist())
+        staff_dist = tuple((space_vals + staff_thick).tolist())
 
-    page.staff_thick = staff_thick
     page.staff_space = staff_space
     page.staff_dist = staff_dist
     return staff_thick, staff_space
