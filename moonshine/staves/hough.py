e1dd5268a98443ad1bc0632ca9253b14dd7d79ec
Dan Ringwalt <dringw@gmail.com>
2014-09-26 10:18:18 -0400

    extend_staff WIP

--- moonshine/staves/hough.py
+++ moonshine/staves/hough.py
@@ -10,7 +10,7 @@ class FilteredHoughStaves(BaseStaves):
         if self.staff_filt is None:
             self.staff_filt = filter.staff_center(self.page)
         thetas = np.linspace(-np.pi/500, np.pi/500, 51)
-        rhores = self.page.staff_thick
+        rhores = (self.page.staff_thick + 1) // 2
         bins = hough.hough_line_kernel(self.staff_filt,
                               rhores=rhores,
                               numrho=self.page.img.shape[0] // rhores,
@@ -43,6 +43,43 @@ class FilteredHoughStaves(BaseStaves):
         for seg in self.get_staff_segments():
             seg = seg.compressed().reshape([-1, 2])
             pylab.plot(seg[:, 0], seg[:, 1], 'y')
+
+    def extend_staff(self, staff_num):
+        # Staff must have at least staff_dist space above and below
+        # to nearest staves
+        staves = self.staves()
+        assert staves
+        staff_min = staves[staff_num,:,1].min() - 3*self.page.staff_dist
+        staff_max = staves[staff_num,:,1].max() + 3*self.page.staff_dist
+        if not ((staves[staff_num-1, :, 1].max()
+                     if staff_num > 0 else 0) + 3*self.page.staff_dist
+                < staff_min < staff_max
+                < (staves[staff_num+1, :, 1].min()
+                        if staff_num + 1 < len(staves)
+                        else self.page.img.shape[0]) - 3*self.page.staff_dist):
+            logging.warn('extend_staff failed: staves too close together')
+            return staves[staff_num]
+
+        thetas = np.linspace(-np.pi/500, np.pi/500, 51)
+        rhores = 1
+        bins = hough.hough_line_kernel(self.staff_filt,
+                              rhores=rhores,
+                              numrho=self.page.img.shape[0] // rhores,
+                              thetas=thetas)
+        peaks = hough.houghpeaks(bins,
+                                 invalidate=(5, self.page.staff_dist // rhores),
+                                 thresh=bins.get().max() / 4.0)
+        theta = thetas[peaks[:, 0]]
+        rho = peaks[:, 1]
+        x0 = 0
+        x1 = self.page.orig_size[1]
+        y0 = (rho*rhores) / np.cos(theta)
+        y1 = (rho*rhores - x1 * np.sin(theta)) / np.cos(theta)
+        for y0val, y1val in zip(y0, y1):
+            pass
+
+        staff_slice = self.nostaff()[staff_min:staff_max].copy()
+        
 
     def get_staves(self):
         self.staves = self.get_staff_segments()
