a4c4b6a58088a369d5d13e616182fcee68c1a09a
Dan Ringwalt <dringw@gmail.com>
2014-10-09 13:17:51 -0400

    Improved staff validation

--- tests/analyze_staff_score.py
+++ tests/analyze_staff_score.py
@@ -3,40 +3,39 @@ import glob
 import gzip
 import re
 
-FILES = glob.glob('tests/validated/*.csv.gz')
+#FILES = glob.glob('tests/validated/*.csv.gz')
+FILES = ['staves.csv.gz']
 staves = pd.DataFrame()
 pages = pd.DataFrame()
 for f in FILES:
-    docname = re.search('IMSLP([0-9]+)', f).group(0)
     doc = pd.DataFrame.from_csv(gzip.open(f))
-    our_staves = doc[~doc['id'].str.contains('page')]
-    our_staves = our_staves[our_staves['score'] >= 0.25]
-    our_staves['doc'] = docname
+    our_staves = doc[~doc.index.to_series().str.contains('page')]
+    page_staves = our_staves[our_staves['score'] >= 0.5]
     staves = staves.append(our_staves)
-    pagenum = our_staves['id'].str.replace('S.+$','') + 'page'
-    staffbypage = our_staves['runs'].groupby(pagenum)
+    pagenum = page_staves.index.to_series().str.replace('S.+$','') + 'page'
+    staffbypage = page_staves['runs'].groupby(pagenum)
     new_page_removed = staffbypage.sum()
     new_page_df = pd.DataFrame(dict(id=new_page_removed.index,
-                                    new_removed=new_page_removed))
-    orig_pages = doc[doc['id'].str.contains('page')]
-    merged = pd.merge(orig_pages, new_page_df, how='outer', on='id')
-    our_pages = merged[['id','new_removed','runs']]
+                                    new_removed=new_page_removed),
+                               index=new_page_removed.index)
+    orig_pages = doc[doc.index.to_series().str.contains('page')]
+    merged = orig_pages.join(new_page_df, how='outer')
+    our_pages = merged[['new_removed','runs']]
     clm = list(our_pages.columns)
-    clm[1] = 'removed'
+    clm[0] = 'removed'
     our_pages.columns = clm
     our_pages['removed'].fillna(0, inplace=True)
     our_pages['score'] = our_pages['removed'] / our_pages['runs']
-    our_pages['doc'] = docname
     pages = pages.append(our_pages)
 
 pages['score'].fillna(0, inplace=True)
 staves['score'].fillna(0, inplace=True)
 
 # can't figure out how to use groupby object
-page_method = pages['id'].str.replace('P.+','')
+page_method = pages.index.to_series().str.replace('-.+','')
 page_bymethod = dict(list(pages['score'].groupby(page_method)))
 
-staff_method = staves['id'].str.replace('P.+','')
+staff_method = staves.index.to_series().str.replace('-.+','')
 staff_bymethod = dict(list(staves['score'].groupby(staff_method)))
 
 import pylab
--- tests/labeled_staff_validation.py
+++ tests/labeled_staff_validation.py
@@ -24,10 +24,15 @@ methods = dict(hough=hough.FilteredHoughStaves,
                #projections=StaffFinder_projections)
 musicstaves_methods = ['fujinaga', 'skeleton']
 
-def kanungo(eta, a0, a, b0, b, k=2, seed=42):
+def kanungo(eta, a0, a, b0, b, k=2, seed=1):
     return lambda x,y: staffdeformation.degrade_kanungo_parallel.__call__(
                             x, y, eta, a0, a, b0, b, k, seed)
-deformations = dict([('k0.01-0.1-0.2', kanungo(0.01, 0.1, 0.2, 0.1, 0.2))])
+deformations = dict([('orig', lambda x,y: [x,y]),
+                     ('k0.02-0.1-0.1', kanungo(0.02, 0.1, 0.1, 0.1, 0.1)),
+                     ('k0.02-0.1-0.1-0.5-0.1', kanungo(0.02, 0.1, 0.1, 0.5, 0.1)),
+                     #('ws0.02-5', lambda x,y: staffdeformation.white_speckles_parallel.__call__(x,y,0.02,5)),
+                     #('typeset', lambda x,y: staffdeformation.typeset_emulation.__call__(x,y,0,0,0)),])
+                     ('curvature', lambda x,y: staffdeformation.curvature.__call__(x,y, 0.02,0.1))])
 
 import gc
 import glob
@@ -36,6 +41,7 @@ import os
 import pandas
 import re
 import shutil
+import signal
 import sys
 import subprocess
 import tempfile
@@ -68,54 +74,67 @@ try:
         nostaff_gamera = gamera.plugins.numpy_io.from_numpy(
                             nostaff_gamera.astype(np.uint16))
 
-        validator = validation.StaffValidation(page)
-        dummy_ = dummy.DummyStaves(page, nostaff)
-        scores = validator.score_staves(method=dummy_)
-        scores.index = ('dummy-%s-' % fileid) + scores.index
-        result = result.append(scores)
-        for method in methods:
-            staves = methods[method](page)
-            scores = validator.score_staves(method=staves)
-            scores.index = ('%s-%s-' % (method, fileid)) + scores.index
-            result = result.append(scores)
-        gamera_image = None
-        for method in musicstaves_methods:
-            staves = methods[method](page, staff_removal='gamera')
-            scores = validator.score_staves(method=staves)
-            scores.index = ('%s-native-%s-' % (method, fileid)) + scores.index
-            result = result.append(scores)
-            if gamera_image is None:
-                gamera_image = staves.gamera_image
-
-        for deformation in deformations:
-            func = deformations[deformation]
+        baselineStaves = None
+        for deformation, func in [('orig', lambda x,y: [x,y])] + list(deformations.iteritems()):
             deformed = func(page_gamera, nostaff_gamera)
             page_d, nostaff_d = deformed[:2]
             page_np = gamera.plugins.numpy_io.to_numpy.__call__(page_d)
+            if False and deformation != 'orig':
+                import pylab
+                pylab.imshow(page_np)
+                pylab.show()
             nostaff_np = gamera.plugins.numpy_io.to_numpy.__call__(nostaff_d)
             page_ = page_mod.Page(page_np)
             nostaff_ = page_mod.Page(nostaff_np)
             page_.staff_dist = page.staff_dist
             page_.staff_thick = page.staff_thick
             page_.staff_space = page.staff_space
+            page_runs = staffsize.light_runs(page_)[page_.staff_space]
 
             validator = validation.StaffValidation(page_)
             dummy_ = dummy.DummyStaves(page_, nostaff_)
+            if deformation == 'orig':
+                baselineStaves = dummy_()
+            else:
+                dummy_.staves = baselineStaves
             scores = validator.score_staves(method=dummy_)
             scores.index = ('dummy-%s-%s-' % (fileid,deformation)) + scores.index
             result = result.append(scores)
+            def handler(signum, frame):
+                raise Exception('...timeout')
             for method in methods:
-                staves = methods[method](page_)
-                scores = validator.score_staves(method=staves)
-                scores.index = ('%s-%s-%s-' % (method, fileid,deformation)) + scores.index
+                print '%s-%s-%s' % (method,fileid,deformation)
+                try:
+                    signal.signal(signal.SIGALRM, handler)
+                    signal.alarm(30)
+                    staves = methods[method](page_)
+                    scores = validator.score_staves(method=staves)
+                    scores.index = ('%s-%s-%s-' % (method, fileid,deformation)) + scores.index
+                    signal.alarm(0)
+                except Exception, e:
+                    signal.alarm(0)
+                    print e
+                    scores = pandas.DataFrame(dict(runs=page_runs,
+                                                   removed=0),
+                                              index=['%s-%s-%s-page' % (method,fileid,deformation)])
                 result = result.append(scores)
             for method in musicstaves_methods:
-                staves = methods[method](page_, staff_removal='gamera')
-                scores = validator.score_staves(method=staves)
-                scores.index = ('%s-native-%s-%s-' % (method, fileid, deformation)) + scores.index
+                print '%s-native-%s-%s' % (method,fileid,deformation)
+                try:
+                    signal.signal(signal.SIGALRM, handler)
+                    signal.alarm(30)
+                    staves = methods[method](page_, staff_removal='gamera')
+                    scores = validator.score_staves(method=staves)
+                    scores.index = ('%s-native-%s-%s-' % (method, fileid,deformation)) + scores.index
+                    signal.alarm(0)
+                except Exception, e:
+                    print e
+                    scores = pandas.DataFrame(dict(runs=page_runs,
+                                                   removed=0),
+                                              index=['%s-%s-%s-page' % (method,fileid,deformation)])
+                finally:
+                    signal.alarm(0)
                 result = result.append(scores)
-
-        break
 
     if len(result):
         result.to_csv(gzip.open(output, 'wb'))
