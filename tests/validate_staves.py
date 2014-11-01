b8d11c577fc4a16bf619bb8bd471662b6046d5ad
Dan Ringwalt <dringw@gmail.com>
2014-11-01 00:41:00 -0400

    validate_staves

--- tests/validate_staves.py
+++ tests/validate_staves.py
@@ -3,7 +3,7 @@ import env
 import moonshine
 from moonshine.staves import validation, hough, path
 from moonshine.staves.gamera_musicstaves import *
-from moonshine.structure import orientation, staffsize
+from moonshine import orientation, staffsize
 
 methods = dict(hough=hough.FilteredHoughStaves,
                #path=path.StablePathStaves,
@@ -14,8 +14,8 @@ methods = dict(hough=hough.FilteredHoughStaves,
                #gamera_simple=MusicStaves_rl_simple,
                skeleton=MusicStaves_skeleton,
                dalitz=StaffFinder_dalitz,
-               )#miyao=StaffFinder_miyao,
-               #projections=StaffFinder_projections)
+               #miyao=StaffFinder_miyao,
+               projections=StaffFinder_projections)
 
 import glob
 import gzip
@@ -32,14 +32,11 @@ try:
     if not (0 < len(glob.glob(tmpdir + '/orig-*.pbm')) <= 50):
         shutil.rmtree(tmpdir)
         sys.exit(0)
-    for orig in glob.glob(tmpdir + '/orig-*.pbm'):
-        newfile = re.sub('orig', 'kanungo', orig)
-        subprocess.call(['/usr/local/bin/QAkanungoDegrade', '-a', '0.5', '-a0', '0.5', '-b', '0.5', '-b0', '0.5', '-eta', '0.0', '-in', orig, '-out', newfile, '-size', '1'])
 
     output = sys.argv[2] if len(sys.argv) > 2 else None
 
     result = pandas.DataFrame()
-    for i, pagename in enumerate(sorted(glob.glob(tmpdir + '/kanungo-*.pbm'))):
+    for i, pagename in enumerate(sorted(glob.glob(tmpdir + '/orig-*.pbm'))):
         page, = moonshine.open(pagename)
 
         staffsize.staffsize(page)
@@ -55,7 +52,7 @@ try:
             if output is None:
                 print scores
             else:
-                scores['id'] = ('%sP%02d' % (method, i)) + scores['id']
+                scores.index = ('%sP%02d' % (method, i)) + scores.index
                 result = result.append(scores)
 
     if output is not None and len(result):
