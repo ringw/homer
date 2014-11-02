7cc51dd02eddd127efaa86c8ab746f65f476b3fa
Dan Ringwalt <dringw@gmail.com>
2014-11-02 16:12:30 -0500

    Updated components for Reikna

--- moonshine/components.py
+++ moonshine/components.py
@@ -6,23 +6,41 @@ prg = build_program('components')
 int2 = np.dtype('i4,i4')
 
 def get_components(classes_img):
-    pixel_tree = cla.zeros(q, classes_img.shape, np.int32)
-    prg.init_component_tree(q, pixel_tree.shape[::-1], (8,8),
-                            classes_img.data, pixel_tree.data)
-    prg.build_component_tree(q, pixel_tree.shape[::-1], (8,8),
-                             classes_img.data, pixel_tree.data)
-    num_components = cla.zeros(q, (1,), np.int32)
-    prg.count_components(q, pixel_tree.shape[::-1], (8,8),
-                         pixel_tree.data, num_components.data)
+    # Pad classes_img as necessary
+    if any(s % 8 != 0 for s in classes_img.shape):
+        classes_img = classes_img.get()
+        pad_h = -(-classes_img.shape[0] & -8)
+        pad_w = -(-classes_img.shape[1] & -8)
+        classes_pad = np.zeros((pad_h, pad_w), np.uint8)
+        classes_pad[:classes_img.shape[0], :classes_img.shape[1]] = classes_img
+        classes_img = thr.to_device(classes_pad)
+
+    pixel_tree = thr.empty_like(Type(np.int32, classes_img.shape))
+    pixel_tree.fill(0)
+    prg.init_component_tree(classes_img.data, pixel_tree.data,
+                            global_size=pixel_tree.shape[::-1],
+                            local_size=(8,8))
+    prg.build_component_tree(classes_img.data, pixel_tree.data,
+                             global_size=pixel_tree.shape[::-1],
+                             local_size=(8,8))
+    num_components = thr.empty_like(Type(np.int32, (1,)))
+    num_components.fill(0)
+    prg.count_components(pixel_tree.data, num_components.data,
+                         global_size=pixel_tree.shape[::-1],
+                         local_size=(8,8))
     num_components = int(num_components.get()[0])
-    bounds = cla.empty(q, (num_components, 4), np.int32)
-    prg.init_component_bounds(q, (num_components,), (1,),
-                              bounds.data,
+    bounds = thr.empty_like(Type(np.int32, (num_components, 4)))
+    prg.init_component_bounds(bounds.data,
                               np.array(classes_img.shape[::-1],
-                                       np.int32).view(int2)[0]).wait()
-    classes = cla.zeros(q, num_components, np.uint8)
-    sums = cla.zeros(q, num_components, np.int32)
-    prg.component_info(q, pixel_tree.shape[::-1], (8, 8),
-                       classes_img.data, pixel_tree.data,
-                       classes.data, bounds.data, sums.data)
+                                       np.int32).view(int2)[0],
+                              global_size=(num_components,),
+                              local_size=(1,))
+    classes = thr.empty_like(Type(np.uint8, num_components))
+    classes.fill(0)
+    sums = thr.empty_like(Type(np.int32, num_components))
+    sums.fill(0)
+    prg.component_info(classes_img.data, pixel_tree.data,
+                       classes.data, bounds.data, sums.data,
+                       global_size=pixel_tree.shape[::-1],
+                       local_size=(8, 8))
     return classes, bounds, sums
