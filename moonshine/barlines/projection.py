7da37ab2edcc0d29e4a6968e69e6399e3e5d0f88
Dan Ringwalt <dringw@gmail.com>
2014-12-21 10:27:23 -0600

    Flexible barline width

--- moonshine/barlines/projection.py
+++ moonshine/barlines/projection.py
@@ -14,19 +14,23 @@ def staff_barlines(page, staff_num):
     staff_proj = img_slice[page.staff_dist*2:page.staff_dist*6, :].sum(0)
     gap_proj = img_slice.sum(0)
 
-    # Barline must take up at least 90% of the vertical space,
+    # Barline must take up at least 80% of the vertical space,
     # and there should be background (few black pixels) around it
     is_barline = staff_proj > page.staff_dist * 4 * 0.8
     is_background = gap_proj < page.staff_dist/2
-    near_background_left = is_background.copy()
-    near_background_right = is_background.copy()
-    for i in range(1, page.staff_dist/2):
-        near_background_left[i:] |= is_background[:-i]
-    for i in range(1, page.staff_dist/2):
-        near_background_right[:-i] |= is_background[i:]
-    is_barline &= near_background_left & near_background_right
     labels, num_labels = util.label_1d(is_barline)
-    barlines = np.rint(util.center_of_mass_1d(labels)).astype(int)
+    if not labels.any():
+        return np.array([], int)
+    barlines = []
+    for label in xrange(1, num_labels+1):
+        pos, = np.where(labels == label)
+        if pos[-1] - pos[0] > page.staff_thick*3:
+            continue
+        left_bg = is_background[max(0, pos[0] - page.staff_dist) : pos[0]]
+        right_bg = is_background[pos[-1] : min(len(gap_proj),
+                                               pos[-1] + page.staff_dist)]
+        if left_bg.any() and right_bg.any():
+            barlines.append(np.mean(pos).astype(int))
 
     # Add a barline at the start and end of the staff if necessary
     if len(barlines):
@@ -35,7 +39,7 @@ def staff_barlines(page, staff_num):
             barlines = np.concatenate([[staff[0,0]], barlines])
         if staff[1,0] - barlines[-1] > page.staff_dist*2:
             barlines = np.concatenate([barlines, [staff[1,0]]])
-    return barlines
+    return np.array(barlines, int)
 
 def get_barlines(page):
     page.barline_filter = page.staves.nostaff()
