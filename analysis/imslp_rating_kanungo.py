f7464f9c4f5aa4dc606b88f0f094f1bf38a33412
Dan Ringwalt <dringw@gmail.com>
2015-03-08 16:57:24 -0400

    Kanungo leave-one-out testing

--- analysis/imslp_rating_kanungo.py
+++ analysis/imslp_rating_kanungo.py
@@ -3,9 +3,9 @@ import cPickle
 import re
 import numpy as np
 import pandas as pd
-import sklearn.linear_model
+import sklearn.svm
 import os.path
-LEARNER = sklearn.linear_model.LinearRegression
+LEARNER = sklearn.svm.SVR
 
 works = cPickle.load(open('imslp/imslp_works.pkl'))
 kanungo = pd.DataFrame.from_csv('results/imslp_kanungo.csv', index_col=range(2))
@@ -29,10 +29,23 @@ ratings['id'] = ratings.index
 regex = re.compile('^' + regex[1:] + '-')
 docs = pd.Series(kanungo.index.get_level_values('doc'), index=kanungo.index)
 kanungo['id'] = docs.str.extract('IMSLP([0-9]+)')
-kanungo = kanungo.merge(ratings, on='id', how='left')
+kanungo = kanungo.merge(ratings, on='id', how='left').set_index(kanungo.index)
 kanungo = kanungo.ix[~ kanungo.isnull().any(1)]
+docs = pd.Series(kanungo.index.get_level_values('doc'), index=kanungo.index)
 X = kanungo[range(6)]
 Y = kanungo['rating']
 
 model = LEARNER()
 model.fit(X, Y)
+
+leave1out = pd.DataFrame()
+for doc in docs.unique():
+    test = docs == doc
+    l1o_model = LEARNER()
+    l1o_model.fit(X.ix[~test], Y[~test])
+    Yreal = Y[test]
+    Ypred = l1o_model.predict(np.array(X.ix[test]))
+    #leave1out.append((Yreal[0], Ypred))
+    leave1out = leave1out.append(zip(Yreal, Ypred))
+err = (leave1out[1] - leave1out[0]).abs()
+print 'mean', err.mean(), 'median', err.median(), 'max', err.max()
