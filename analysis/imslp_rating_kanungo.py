18f59dbfba465ce514b0b5eb3bdaf3853b09c060
Dan Ringwalt <dringw@gmail.com>
2015-03-12 13:58:29 -0400

    Image quality features from deskewing

--- analysis/imslp_rating_kanungo.py
+++ analysis/imslp_rating_kanungo.py
@@ -1,11 +1,13 @@
 # Fit IMSLP user ratings to the estimated Kanungo degradation of the score.
 import cPickle
+import glob
+import gzip
 import re
 import numpy as np
 import pandas as pd
-import sklearn.svm
+import sklearn.linear_model
 import os.path
-LEARNER = sklearn.svm.SVR
+LEARNER = lambda: sklearn.linear_model.Lasso(alpha=0.01, normalize=True)
 
 works = cPickle.load(open('imslp/imslp_works.pkl'))
 kanungo = pd.DataFrame.from_csv('results/imslp_kanungo.csv', index_col=range(2))
@@ -16,6 +18,25 @@ kanungo['a'] = kanungo['a'].clip(0, np.inf)
 kanungo['b0'] = kanungo['b0'].clip(0, 1)
 kanungo['b'] = kanungo['b'].clip(0, np.inf)
 kanungo['k'] = np.rint(kanungo['k'].clip(0, 5))
+feats = kanungo.copy()
+
+deskew = sorted(glob.glob('results/imslp_deskew/*.csv.gz'))
+deskew_feats = pd.DataFrame()
+for f in deskew:
+    result = pd.DataFrame.from_csv(gzip.open(f), index_col=range(2))
+    left = (result.staff_left / 32 + 1).round()
+    right = (result.staff_right / 32 - 1).round()
+    skews = result[range(4, result.shape[1])]
+    skews.columns = range(skews.shape[1])
+    skews = skews.apply(lambda x: x[int(left.ix[x.name]):int(right.ix[x.name])],
+                        axis=1)
+    skewdiff = skews.T.diff().T.abs()
+    deskew_feats = deskew_feats.append(pd.DataFrame(dict(
+                      mean_skew_norm=skewdiff.mean(1) / result.staff_dist,
+                      median_skew_norm=skewdiff.median(1) / result.staff_dist,
+                      staff_dist=result.staff_dist,
+                      staff_thick_ratio=result.staff_thick / result.staff_dist)))
+feats = feats.join(deskew_feats, how='left')
 
 ratings = pd.DataFrame(columns=['rating'])
 regex = ''
@@ -27,13 +48,13 @@ for title, work in works.iteritems():
             regex += '|IMSLP' + score_id
 ratings['id'] = ratings.index
 regex = re.compile('^' + regex[1:] + '-')
-docs = pd.Series(kanungo.index.get_level_values('doc'), index=kanungo.index)
-kanungo['id'] = docs.str.extract('IMSLP([0-9]+)')
-kanungo = kanungo.merge(ratings, on='id', how='left').set_index(kanungo.index)
-kanungo = kanungo.ix[~ kanungo.isnull().any(1)]
-docs = pd.Series(kanungo.index.get_level_values('doc'), index=kanungo.index)
-X = kanungo[range(6)]
-Y = kanungo['rating']
+docs = pd.Series(kanungo.index.get_level_values('doc'), index=feats.index)
+feats['id'] = docs.str.extract('IMSLP([0-9]+)')
+feats = feats.merge(ratings, on='id', how='left').set_index(feats.index)
+feats = feats.ix[~ feats.isnull().any(1)]
+docs = pd.Series(feats.index.get_level_values('doc'), index=feats.index)
+X = feats[range(10)]
+Y = feats['rating']
 
 model = LEARNER()
 model.fit(X, Y)
