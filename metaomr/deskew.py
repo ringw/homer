874f615c535f3d0c6919b9743e770ccf74885f8a
Dan Ringwalt <dringw@gmail.com>
2015-03-17 07:29:27 -0400

    Deskew fixes

--- metaomr/deskew.py
+++ metaomr/deskew.py
@@ -1,8 +1,7 @@
 # Fujinaga's deskewing algorithm
 import numpy as np
 from metaomr import bitimage
-def get_strip_offsets(page):
-    strip_size = 32
+def get_strip_offsets(page, strip_size=32):
     strips = get_strips(page, strip_size)
     center = strips.shape[1] / 2
     center_strip = strips[:, center]
@@ -16,20 +15,20 @@ def get_strip_offsets(page):
         center_strip += np.roll(strips[:, s], offset)
         strip_offsets[s] = offset
     return strip_offsets
-def get_column_offsets(page):
-    strip_offsets = get_strip_offsets(page)
+def get_column_offsets(page, strip_size=32):
+    strip_offsets = get_strip_offsets(page, strip_size)
     column_offsets = np.zeros(page.orig_size[1], int)
     column_offsets[:strip_size/2] = strip_offsets[0]
-    for s in xrange(strips.shape[1] - 1):
+    for s in xrange(len(strip_offsets) - 1):
         column_offsets[s*strip_size + strip_size/2:(s+1)*strip_size + strip_size/2] = \
             np.rint(np.linspace(strip_offsets[s], strip_offsets[s+1], strip_size)).astype(int)
     column_offsets[-strip_size/2:] = strip_offsets[-1]
     return column_offsets
 def deskew(page):
-    column_offsets = deskew_offsets(page)
+    column_offsets = get_column_offsets(page)
     for col in xrange(page.orig_size[1]):
         page.byteimg[col] = np.roll(page.byteimg[col], column_offsets[col])
-    page.img = bitimage.as_hostimage(page.byteimg)
+    page.img = bitimage.as_bitimage(page.byteimg)
 
 def get_strips(page, strip_size):
     width = -(-page.orig_size[1] / strip_size) * strip_size
