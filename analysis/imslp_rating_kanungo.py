792123ee126ef6123b503073ff39b35c1c26c53b
Dan Ringwalt <dringw@gmail.com>
2015-03-17 07:29:17 -0400

    imslp_rating_kanungo

--- analysis/imslp_rating_kanungo.py
+++ analysis/imslp_rating_kanungo.py
@@ -7,13 +7,14 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model
 import os.path
-LEARNER = lambda: sklearn.linear_model.Lasso(alpha=0.01, normalize=True)
+#LEARNER = lambda: sklearn.linear_model.Lasso(alpha=0.01, normalize=True)
+LEARNER = sklearn.linear_model.LinearRegression
 
 works = cPickle.load(open('imslp/imslp_works.pkl'))
 kanungo = pd.DataFrame()
 for f in sorted(glob.glob('results/imslp_kanungo/*.csv')):
-    results = pd.DataFrame.from_csv(f, index_col=range(2))
-    kanungo = kanungo.append(results)
+    results = pd.DataFrame.from_csv(f, index_col=range(3))
+    kanungo = kanungo.append(results.xs('ks', level='fn'))
 kanungo = kanungo['nu a0 a b0 b k'.split()]
 kanungo['nu'] = kanungo['nu'].clip(0, 1)
 kanungo['a0'] = kanungo['a0'].clip(0, 1)
@@ -58,20 +59,21 @@ feats['id'] = docs.str.extract('IMSLP([0-9]+)')
 feats = feats.merge(ratings, on='id', how='left').set_index(feats.index)
 feats = feats.ix[~ feats.isnull().any(1)]
 docs = pd.Series(feats.index.get_level_values('doc'), index=feats.index)
-X = feats[range(10)]
+X = feats[range(12)]
 Y = feats['rating']
 
 model = LEARNER()
 model.fit(X, Y)
+Ypred = model.predict(X)
 
-leave1out = pd.DataFrame()
-for doc in docs.unique():
-    test = docs == doc
-    l1o_model = LEARNER()
-    l1o_model.fit(X.ix[~test], Y[~test])
-    Yreal = Y[test]
-    Ypred = l1o_model.predict(np.array(X.ix[test]))
-    #leave1out.append((Yreal[0], Ypred))
-    leave1out = leave1out.append(zip(Yreal, Ypred))
-err = (leave1out[1] - leave1out[0]).abs()
-print 'mean', err.mean(), 'median', err.median(), 'max', err.max()
+Yperm = np.array(Y)
+np.random.shuffle(Yperm)
+nullmodel = LEARNER()
+nullmodel.fit(X, Yperm)
+
+Xval = np.array(X)
+errs = ((Ypred - Y) ** 2).sum().astype(float) / (len(Y)-2)
+SE = np.sqrt(errs / ((Xval - Xval.mean(0))**2).sum(0))
+T = model.coef_ / SE
+from scipy.stats import t as tdist
+p = 2*(1-tdist.cdf(T, df=len(Y)-2))
