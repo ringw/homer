a4e6b1a094f44335629259066acb6f88aa71a936
Dan Ringwalt <dringw@gmail.com>
2016-10-13 00:22:18 -0400

    Iterative sublists

--- homer/staves/base.py
+++ homer/staves/base.py
@@ -16,21 +16,21 @@ class BaseStaffDetector(object):
     for i in range(n):
       img = filt[:, i*increment:(i+1)*increment if i + 1 < n else None]
       staff_sections.append(self.detect(img))
-    result, result2 = tf.py_func(join_staves, [page.staff_dist] + staff_sections, [staff_sections[0].dtype, staff_sections[1].dtype])
-    return result, result2
+    results = tf.py_func(join_staves, [page.staff_dist] + staff_sections, [staff_sections[0].dtype] * n)
+    return results
 
 def join_staves(staff_dist, *sections):
-  last_staves = [sections[0]]
+  staves_sublists = [sections[0][:, None]]
   for cur_staves in sections[1:]:
-    dist = np.abs(last_staves[-1][None, :] - cur_staves[:, None])
+    dist = np.abs(staves_sublists[-1][None, :, -1] - cur_staves[:, None])
     did_match = dist.min(axis=1) < staff_dist
     cur_staves = cur_staves[did_match]
     matches = np.argmin(dist[did_match, :], axis=1)
     matches, idx = np.unique(matches, return_index=True)
     cur_staves = cur_staves[idx]
-    last_staves = [s[matches] for s in last_staves]
-    last_staves.append(cur_staves)
-    return last_staves
+    new_staves = np.c_[staves_sublists[-1][matches, :], cur_staves]
+    staves_sublists.append(new_staves)
+  return staves_sublists
 
 def staff_center_filter(page):
   img = page.tensor
